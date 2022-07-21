e5f2d019c73ab2bd5ac08d8d0db196ddc2ba77d1
Harshit <harshit1032@gmail.com>
2022-07-21 14:21:01 +0530

    Added a new file

--- recursion/permutations.cpp
+++ recursion/permutations.cpp
@@ -1,7 +1,9 @@
+// Program to clacualte all the permutations of a string 
+
 #include<bits/stdc++.h>
 using namespace std;
 
-void permute(string s, int i)
+void permute(string &s, int i) //i keeps the track of the index from which swaping starts i.e. string before i is already fixed 
 {
 	if(i == s.size()-1)
 	{
@@ -9,16 +11,16 @@ void permute(string s, int i)
 		return;
 	}
 
-	for(int j=i;j<s.size();j++)
+	for(int j=i;j<s.size();j++)  
 	{
-		swap(s[i],s[j]);
-		permute(s,i+1);
-		swap(s[i],s[j]);
+		swap(s[i],s[j]); //index i and j are swapped 
+		permute(s,i+1); //function is called recusively for every index starting from i to the last index
+		swap(s[i],s[j]); //i and j are again swapped to retrun the stirng back to the previous state (backtracking)
 	}
 }
 
 int main()
 {
-	string s = "ABC";
+	string s = "ABCD";
 	permute(s,0);
 }
